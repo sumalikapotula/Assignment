187931a18bc4e2fa9d507c9b5f12b7fe726535d8
sumalikapotula <68680647+sumalikapotula@users.noreply.github.com>
2020-08-04 21:21:17 +0530

    Delete casedisplay.sh

--- casedisplay.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-read -p "enter the number : " a
-case $a in
-	1) echo "ones" ;;
-        10) echo " tens" ;;
-	100) echo "hundreds" ;;
-	1000) echo "thousands" ;;
-        *) echo "not processed" ;;	
-esac
